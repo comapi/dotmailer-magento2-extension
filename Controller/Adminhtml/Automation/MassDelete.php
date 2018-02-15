594e0257aeda2b1ead4de4a81ff96f56ebc763fa
adeelq <adeel.qamar@dotmailer.com>
2018-02-15 15:36:23 +0100

    architect review

--- Controller/Adminhtml/Automation/MassDelete.php
+++ Controller/Adminhtml/Automation/MassDelete.php
@@ -73,12 +73,4 @@ class MassDelete extends \Magento\Backend\App\Action
 
         return $resultRedirect->setPath('*/*/');
     }
-
-    /**
-     * @return bool
-     */
-    protected function _isAllowed()
-    {
-        return $this->_authorization->isAllowed('Dotdigitalgroup_Email::automation');
-    }
 }
